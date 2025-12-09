f5723955a01333edd284fe3c60c37a5ce06b41d7
pymesayaa~ <ivett.pribula@gmail.com>
2025-12-09 20:35:04 +0100

    Refactor original source code to make tests run without OOM (#18)
    * efactor: make deploy.py import-safe for tests.
    
    * Refactor: model_select.py for  making test import-safe
    
    * Refactor: preprocessing.py  for import-safe testing
    
    * Refactor: training.py to  be more readable and import-safe for testing

--- mlops_project/model_select.py
+++ mlops_project/model_select.py
@@ -12,63 +12,77 @@ artifact_path = "model"
 model_name = "lead_model"
 experiment_name = current_date
 
+
 def wait_until_ready(model_name, model_version):
     client = MlflowClient()
     for _ in range(10):
         model_version_details = client.get_model_version(
-          name=model_name,
-          version=model_version,
+            name=model_name,
+            version=model_version,
         )
         status = ModelVersionStatus.from_string(model_version_details.status)
         if status == ModelVersionStatus.READY:
             break
         time.sleep(1)
 
-experiment_ids = [mlflow.get_experiment_by_name(experiment_name).experiment_id]
 
-experiment_best = mlflow.search_runs(
-    experiment_ids=experiment_ids,
-    order_by=["metrics.f1_score DESC"],
-    max_results=1
-).iloc[0]
-experiment_best
+def main():
+    experiment_ids = [mlflow.get_experiment_by_name(experiment_name).experiment_id]
 
-with open("./artifacts/model_results.json", "r") as f:
-    model_results = json.load(f)
-results_df = pd.DataFrame({model: val["weighted avg"] for model, val in model_results.items()}).T
+    experiment_best = mlflow.search_runs(
+        experiment_ids=experiment_ids, order_by=["metrics.f1_score DESC"], max_results=1
+    ).iloc[0]
+    experiment_best
 
-best_model = results_df.sort_values("f1-score", ascending=False).iloc[0].name
+    with open("./artifacts/model_results.json", "r") as f:
+        model_results = json.load(f)
+    results_df = pd.DataFrame({model: val["weighted avg"] for model, val in model_results.items()}).T
 
-client = MlflowClient()
-prod_model = [model for model in client.search_model_versions(f"name='{model_name}'") if dict(model)['current_stage']=='Production']
-prod_model_exists = len(prod_model)>0
+    best_model = results_df.sort_values("f1-score", ascending=False).iloc[0].name
 
-if prod_model_exists:
-    prod_model_version = dict(prod_model[0])['version']
-    prod_model_run_id = dict(prod_model[0])['run_id']
+    client = MlflowClient()
+    prod_model = [
+        model
+        for model in client.search_model_versions(f"name='{model_name}'")
+        if dict(model)["current_stage"] == "Production"
+    ]
+    prod_model_exists = len(prod_model) > 0
 
-train_model_score = experiment_best["metrics.f1_score"]
-model_details = {}
-model_status = {}
-run_id = None
+    if prod_model_exists:
+        # prod_model_version = dict(prod_model[0])['version'] not in use in the codebase
+        prod_model_run_id = dict(prod_model[0])["run_id"]
 
-if prod_model_exists:
-    data, details = mlflow.get_run(prod_model_run_id)
-    prod_model_score = data[1]["metrics.f1_score"]
+    train_model_score = experiment_best["metrics.f1_score"]
+    model_details = {}
+    model_status = {}
+    run_id = None
 
-    model_status["current"] = train_model_score
-    model_status["prod"] = prod_model_score
+    if prod_model_exists:
+        data, details = mlflow.get_run(prod_model_run_id)
+        prod_model_score = data[1]["metrics.f1_score"]
 
-    if train_model_score>prod_model_score:
+        model_status["current"] = train_model_score
+        model_status["prod"] = prod_model_score
+
+        if train_model_score > prod_model_score:
+            run_id = experiment_best["run_id"]
+    else:
         run_id = experiment_best["run_id"]
-else:
-    run_id = experiment_best["run_id"]
 
-if run_id is not None:
-    model_uri = "runs:/{run_id}/{artifact_path}".format(
-        run_id=run_id,
-        artifact_path=artifact_path
-    )
-    model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
-    wait_until_ready(model_details.name, model_details.version)
-    model_details = dict(model_details)
+    if run_id is not None:
+        model_uri = "runs:/{run_id}/{artifact_path}".format(run_id=run_id, artifact_path=artifact_path)
+        model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
+        wait_until_ready(model_details.name, model_details.version)
+        model_details = dict(model_details)
+
+    return {
+        "best_model": best_model,
+        "experiment_best": experiment_best,
+        "model_status": model_status,
+        "run_id": run_id,
+        "model_details": model_details,
+    }
+
+
+if __name__ == "__main__":
+    main()
