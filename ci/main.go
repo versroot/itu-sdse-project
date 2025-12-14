2aa62bdcc509e0f6268acb8196104bbbc2a7abc8
py-mesayaa <ivett.pribula@gmail.com>
2025-12-14 15:35:24 +0100

    fix: update Dagger pipeline to exclude specific directories and adjust model export path

--- ci/main.go
+++ ci/main.go
@@ -19,24 +19,27 @@ func main() {
 	// initialization of Dagger client
 	ctx := context.Background() // root context
 
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr)) 
-	
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+
 	// error handling
 	if err != nil {
 		log.Fatalf("failed to connect to Dagger: %v", err)
 	}
 	defer client.Close()
 
-	// repo root as build context (directory) 
-	src := client.Host().Directory("..") // host is current dir, ".." goes one level up from ci/ to repo root
+	// repo root as build context (directory)
+	src := client.Host().Directory(".", dagger.HostDirectoryOpts{
+		Exclude: []string{".venv", "venv", "__pycache__", "mlruns", "artifacts"},
+	})
+	// host is current dir, ".." goes one level up from ci/ to repo root
 
 	// create container from Dockerfile in repo root, set workdir to /app
 	cont := src.DockerBuild().WithWorkdir("/app")
 
-	// sync dependencies with UV 
+	// sync dependencies with UV
 	cont = cont.WithExec([]string{"bash", "-lc", "uv sync"}) // -lc load shell and run command; uv sync installs dependencies from pyproject.toml
-    
-    // actual pipeline steps
+
+	// actual pipeline steps
 	// helper function mustRunStep runs each step and handles errors
 	// implementation of mustRunStep is below main()
 
@@ -59,19 +62,20 @@ func main() {
 	// 5) package model into /app/model
 	cont = mustRunStep(ctx, cont, "package_model",
 		"mkdir -p model && "+
-			"cp artifacts/lead_model_lr.pkl model/model.pkl && "+  
+			"cp artifacts/lead_model_lr.pkl model/model.pkl && "+
 			"cp artifacts/columns_list.json model/columns_list.json && "+
 			"cp artifacts/scaler.pkl model/scaler.pkl",
 	)
 
 	// 6) export /app/model -> ../model (repo root)
-	_, err = cont.Directory("/app/model").Export(ctx, "../model")
+	_, err = cont.Directory("/app/model").Export(ctx, "model")
 	if err != nil {
 		log.Fatalf("failed to export model artifact: %v", err)
 	}
 
 	log.Println("Dagger pipeline finished successfully. Model exported to ./model in repo root.")
 }
+
 // helper function to run a pipeline step and handle errors
 func mustRunStep(ctx context.Context, c *dagger.Container, name, cmd string) *dagger.Container {
 	log.Printf("\n=== Running step: %s ===\n", name)
